ca458fdf3b0a3e95d192f93932fa7dd90c624294
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 17:01:05 +0800

    加入專案檔案。

---
+++ Exec_DeclareClassMembers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class FileManager
+    {
+        public string Save(string filenNmes, string ADD)
+        {
+            return null;
+        }
+    }
+}
